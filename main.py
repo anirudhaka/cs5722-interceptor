577b3233aafe99fcafe96f67638c9b1b599fa04f
Anirudha K A <anirudha.ka04@gmail.com>
2023-02-18 10:12:35 +0000

    update client code to use dispatcher

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ The client code
 """
 
 from application import Application
-from basic_interceptor import BasicInterceptor
+# from basic_interceptor import BasicInterceptor
+from interceptor_manager_dispatcher import InterceptorManagerDispatcher
+from logging_interceptor import LoggingInterceptor
 
 def main():
-    interceptor = BasicInterceptor(Application())
-    interceptor.execute("request")
+    dispatcher = InterceptorManagerDispatcher(Application())
+
+    log_int = LoggingInterceptor()
+    dispatcher.add(log_int)
+    dispatcher.execute("test request")
 
 if __name__ == "__main__":
     main()
