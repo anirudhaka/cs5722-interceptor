c04b7ddadaccd76f829548d3beaf2ec7c8d48069
Anirudha K A <anirudha.ka04@gmail.com>
2023-02-26 23:13:39 +0000

    feat: added statement call in the interceptor

--- statement_interceptor.py
+++ statement_interceptor.py
@@ -12,5 +12,6 @@ class SaveStatementInterceptor(Interceptor):
         time_now = str(context.get_creation_time())
         print("The customer name: ", context.customer_object.get_name())
         print("The rentals: ", context.customer_object.get_rentals())
+        print("The statement is:\n ", context.customer_object.statement())
         print(f"Saving the statement as {customer_name +'_'+ time_now} ")
         # add save code
