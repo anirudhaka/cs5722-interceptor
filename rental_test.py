56d06674226054c5b50cae36670d1c715e6f2f59
Anirudha K A <anirudha.ka04@gmail.com>
2023-02-26 01:17:03 +0000

    test: add new test for frequent renter points

--- rental_test.py
+++ rental_test.py
@@ -29,6 +29,12 @@ class RentalTest(unittest.TestCase):
 		self.assertEqual(rental.get_charge(), 7.5)
 		# self.fail("TODO add more tests for other movie categories")
 
-	@unittest.skip("TODO add test of frequent renter points when you add it to Rental")
+	# @unittest.skip("TODO add test of frequent renter points when you add it to Rental")
 	def test_rental_points(self):
-		self.fail("TODO add  test of frequent renter points")
+		rental = Rental(self.new_movie, 2)
+		self.assertEqual(rental.get_frequent_renter_points(), 2)
+		rental = Rental(self.new_movie, 1)
+		self.assertEqual(rental.get_frequent_renter_points(), 1)
+		rental = Rental(self.childrens_movie, 10)
+		self.assertEqual(rental.get_frequent_renter_points(), 1)
+		# self.fail("TODO add  test of frequent renter points")
